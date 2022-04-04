2a16adb0cffcb88c68e8eb71fc741670be5b08d4
Nosferatu <davonb113@gmail.com>
2022-04-04 17:31:11 -0500

    commit

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-const profileDataArgs = process.argv.slic(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-const printProfileData = (profileDataArr) => {
-    for( let i = 0; i < profileDataArr.length; i++){
-        console.log(profileDataArr[i]);
-    } 
+const profileDataArgs = process.argv.slice(2);
 
-    console.log('============');
+const [name, github] = profileDataArgs;
 
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem);
-    });
-};
+const generatePage = require('./src/page-template.js');
 
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name,github), err => {
+    if (err) throw err;
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
