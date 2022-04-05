47cd2e9ac11029f282b7e690e9a26c8b2b102546
Nosferatu <davonb113@gmail.com>
2022-04-04 21:34:04 -0500

    added inquirer

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+    .prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }]).then(answers => console.log(answers));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
+// const [name, github] = profileDataArgs;
 
-const generatePage = require('./src/page-template.js');
-
-fs.writeFile('index.html', generatePage(name,github), err => {
-    if (err) throw err;
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// fs.writeFile('index.html', generatePage(name,github), err => {
+//     if (err) throw err;
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
